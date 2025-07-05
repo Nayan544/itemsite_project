82b56e4363f49fd6db0d4a962528c2ccf48fed07
Nayan Dabhi <nayandabhi544@gmail.com>
2025-07-05 12:31:49 +0530

    add python-domain new line

--- itemsite/settings.py
+++ itemsite/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'itemsite-project.onrender.com',
-    'www.your-custom-domain.com',
+    'www.nayan544.pythonanywhere.com',
     'localhost',  # optional for local dev
     '127.0.0.1',  # optional for local dev
     'nayanshop.com','nayanshop.com',
